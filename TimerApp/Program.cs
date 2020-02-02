bf4b20c1773d2d6f34a052b7984b131d8567559d
delil97 <delil.demirsoy@hotmail.com>
2020-02-02 13:50:04 +0100

    MyTimerProgram

---
+++ TimerApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TimerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Timer mytime = new Timer(new TimerCallback(TheTime), null, 0, 1000);
+            Console.ReadLine();
+
+          
+
+        }
+
+            static void TheTime(object time)
+            {
+                Console.WriteLine(DateTime.Now.ToString("h: mm:ss"));
+            }
+
+        }
+    }
+
